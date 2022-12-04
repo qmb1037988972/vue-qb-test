30bfa3fa01fb49e1f6d3edf49ef27e9be2015802
qmb1037988972 <qmd1037988972@163.com>
2022-12-04 16:31:24 +0800

    组件交互

---
+++ vue-ex/src/components/InteractiveCom.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="interactive-com">
+    <input type="text" :value="value">
+    <button @click="add">add</button>
+    <div>
+      <button @click="changePer">changePer</button>
+      <button @click="changeDataPer">changeDataPer</button>
+      <br>
+      {{ perList[0] }},{{ perList[1] }},{{ perList[0] + perList[1] }}
+      {{ perList }}
+      {{ perObj.name }}, {{ perObj.age }}, {{ perObj }}
+      <br />
+      -------------------------------
+      <br />
+      {{ dataPerList[0] }},{{ dataPerList[1] }},{{ dataPerList[0] + dataPerList[1] }}
+      {{ dataPerList }}
+      {{ dataPerObj.name }}, {{ dataPerObj.age }}, {{ dataPerObj }}
+      <br />
+      -------------------------------
+      <br />
+      {{ parsePerList[0] }},{{ parsePerList[1] }},{{ parsePerList[0] + parsePerList[1] }}
+      {{ parsePerList }}
+      {{ parsePerObj.name }}, {{ parsePerObj.age }}, {{ parsePerObj }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InteractiveCom',
+  props: {
+    value: {
+      type: Number
+    },
+    perList: {
+      type: Array
+    },
+    perObj: {
+      type: Object
+    },
+    perListTow: {
+      type: Array
+    }
+  },
+  mounted() {
+    console.log(this.value, 'value')
+    console.log(this.perList, 'perList')
+    console.log(this.perObj, 'Object')
+  },
+  data() {
+    return {
+      dataNum: this.num,
+      dataPerList: this.perList,
+      dataPerObj: this.perObj,
+      parsePerObj: JSON.parse(JSON.stringify(this.perObj)),
+      parsePerList: JSON.parse(JSON.stringify(this.perList))
+    }
+  },
+  model: {
+    event: 'change',
+    prop: 'value'
+  },
+  methods: {
+    add() {
+      // this.dataNum++
+      this.$emit('add')
+    },
+    changePer() {
+      this.$emit('changePer')
+    },
+    changeDataPer() {
+      this.dataPerList[0] = this.perList[0] + 2
+      this.dataPerObj.name = 'qb'
+      this.dataPerObj.age = this.perObj.age + 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ vue-ex/src/page/ExHome.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <interactive-com
+      v-model="num"
+      @add="add"
+      @changePer="changePer"
+      :perList="perList"
+      :perObj="perObj"
+      :perListTwo="perList2"
+    />
+    <input type="text" v-model="num">
+    <div>
+      {{ perList[0] }},{{ perList[1] }},{{ perList[0] + perList[1] }}
+      {{ perList }}
+      <button @click="changePer">changePer</button>
+      {{ perObj.name }}, {{ perObj.age }}, {{ perObj }}
+    </div>
+  </div>
+</template>
+
+<script>
+import InteractiveCom from '../components/InteractiveCom.vue'
+export default {
+  name: 'ExHome',
+  components: {
+    InteractiveCom
+  },
+  data() {
+    return {
+      num: 1,
+      perList: [4, 5, 9, 11],
+      perObj: {
+        name: 'xiaoming',
+        age: 13
+      },
+      perList2: [
+        {
+          name: 'xiaohong',
+          age: 14
+        },
+        {
+          name: 'xiaodong',
+          age: 18
+        }
+      ]
+    }
+  },
+  methods: {
+    add() {
+      this.num++
+    },
+    changePer() {
+      this.perList[0] = this.perList[0] + 2
+      this.perObj.name = 'qb'
+      this.perObj.age = this.perObj.age + 1
+    }
+  }
+}
+</script>
+
+<style>
+</style>
