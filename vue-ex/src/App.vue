b77e15f4b15308eef35ea12d356129188861dbe4
qmb1037988972 <qmd1037988972@163.com>
2023-09-14 17:08:03 +0800

    jfsdlk

--- vue-ex/src/App.vue
+++ vue-ex/src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div>
     <!-- <home></home> -->
-    <if-test></if-test>
+    <!-- <if-test></if-test> -->
+    <tableText></tableText>
+    /**
+      近代背景
+      人妖共生的世界，但是因为历史的断代，没人知道人妖为什么一起生活在这个世界。而洛老就是在调查这一切。
+      
+
+
+     */
   </div>
 </template>
 
 <script>
 // import Home from './page/ExHome.vue'
-import IfTest from './page/IfTest.vue'
+// import IfTest from './page/IfTest.vue'
+import tableText from './page/tableText.vue'
 export default {
   name: 'App',
   components: {
     // Home,
-    IfTest,
+    // IfTest,
+    tableText
   }
 }
 </script>
